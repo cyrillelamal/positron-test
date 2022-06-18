c709cff63a8fc019a42a6b7b21d80559d2f07519
Cyrille <cyril2lambda@gmail.com>
2022-06-18 16:51:17 +0300

    Send email after submitting feedback form

--- tests/Controller/FeedbackControllerTest.php
+++ tests/Controller/FeedbackControllerTest.php
@@ -30,4 +30,19 @@ class FeedbackControllerTest extends WebTestCase
 
         $this->assertSelectorExists('input[name="feedback_form[captcha]"]');
     }
+
+    public function testItSendsAnEmailNotificationBackToUser(): void
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request(Request::METHOD_GET, self::URI);
+        $form = $crawler->filter(self::FORM_SELECTOR)->form();
+        $form->setValues([
+            'feedback_form[email]' => 'foo@bar.test',
+            'feedback_form[message]' => 'cc',
+        ]);
+
+        $client->submit($form);
+        $this->assertEmailCount(1);
+    }
 }
